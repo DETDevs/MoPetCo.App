bd32cb12cb597b62399e4f643814b63a9b793af5
edwin08torres <at2899743@gmail.com>
2025-03-16 04:01:18 -0600

    t-devp: add HomePage

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,14 @@
 import { Footer } from "./components/Footer";
 import { Header } from "./components/Header";
+import { HomePage } from "./pages/HomePage";
 
 export const App = () => {
   return (
     <div className="flex flex-col min-h-screen">
       <Header />
 
-      <main className="flex-grow">
-        <section className="p-4 text-center">
-          <h1 className="text-2xl font-bold text-blue-600 p-20">
-            Bienvenido a MoPetCo
-          </h1>
-        </section>
+      <main>
+        <HomePage />
       </main>
 
       <Footer />
