6bff73fbd028fb229d14adfaa44baa77450d88ed
edwin08torres <at2899743@gmail.com>
2025-03-19 20:21:21 -0600

    t-devp: upd tamano de letra para la seccion service

--- src/components/ui/Home/ServiceSection.tsx
+++ src/components/ui/Home/ServiceSection.tsx
@@ -4,10 +4,10 @@ export const ServiceSection = () => {
   return (
     <div className="flex flex-col md:flex-row justify-center w-full items-center my-12 ">
       <div className="flex justify-center items-center flex-col w-1/2 lg:w-1/2 text-center lg:text-left space-y-4 mb-12 md:mb-0">
-        <h2 className="text-4xl md:text-5xl font-bold lg:text-6xl">
+        <h2 className="text-4xl md:text-4xl text-left font-bold lg:text-4xl xl:text-6xl">
           Services Pet Care with Love
         </h2>
-        <p className="text-gray-600 w-[22rem] md:w-full lg:w-[45rem] lg:text-2xl">
+        <p className="text-gray-600 w-[22rem] md:w-full lg:w-[30rem] lg:text-xl xl:w-[45rem] xl:text-2xl">
           All grooming options include a complete organic and hypoallergenic
           bath as well as the thorough nose-to-tail attention outlined below.
           Your groomer will have a full consultation with you before getting
